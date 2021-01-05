47bc1cabd598aba7362e90ffff5f8d26c19fdca7
Géssica Mendonça <$ git status>
2021-01-04 21:29:45 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/devgm/gmdelivery/GmdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devgm.gmdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devgm/gmdelivery/GmdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgm.gmdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
