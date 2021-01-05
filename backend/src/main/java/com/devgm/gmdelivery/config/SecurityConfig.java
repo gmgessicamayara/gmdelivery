11626b82a56828e6fb2413808c6e9f84ee90c68d
Géssica Mendonça <$ git status>
2021-01-05 09:21:35 -0300

    Modelos de Dominio

--- backend/src/main/java/com/devgm/gmdelivery/config/SecurityConfig.java
+++ backend/src/main/java/com/devgm/gmdelivery/config/SecurityConfig.java
@@ -23,13 +23,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
-		
-		http.cors().and().csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		 http.authorizeRequests()
+         .antMatchers("/").permitAll()
+         .antMatchers("/h2-console/**").permitAll();
+
+		 	http.csrf().disable();
+		 	http.headers().frameOptions().disable();
 	}
 
 	@Bean
@@ -40,4 +39,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+	
 }
---
+++ backend/src/main/java/com/devgm/gmdelivery/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.devgm.gmdelivery.entities;
+
+public enum OrderStatus {	
+	PENDING, DELIVERED;
+}
