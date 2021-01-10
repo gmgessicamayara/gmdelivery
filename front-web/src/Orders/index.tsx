c6c91c421aa4bf7994f5f820cf0030f605a1a6a7
Géssica Mendonça <$ git status>
2021-01-10 10:03:02 -0300

    Implementação do mapa para localização dos endereços de pedidos

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,12 +2,14 @@ import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductList from './ProductList';
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] =  useState<OrderLocationData>();
 
     useEffect( () => {
        fetchProducts()
@@ -19,11 +21,8 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader/>
             <ProductList products={products}/>
-            
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
-       
-      
     );
 }
-
 export default Orders;
