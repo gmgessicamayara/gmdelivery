60ff58da21cebc85e6c71499bf444c645dd45f0b
Géssica Mendonça <$ git status>
2021-01-10 10:55:08 -0300

    Implementação da seleção do produtos

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -4,6 +4,8 @@ import { Product } from './types';
 
 type Props = {
     product: Product;
+    onSelectProduct: (product: Product) => void;
+    isSelected: boolean;
 }
 
 function formatPrice(price: number){
@@ -16,9 +18,10 @@ function formatPrice(price: number){
     return formatter.format(price);
 }
 
-function ProductCard({ product }: Props) {
+function ProductCard({ product, onSelectProduct, isSelected }: Props) {
     return (
-       <div className="order-card-container">
+       <div className={`order-card-container ${isSelected ? 'selected' : '' }`}
+       onClick={() => onSelectProduct(product)}  >
               <h3 className="order-card-title">
               { product.name }
               </h3>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -5,9 +5,13 @@ import { useEffect, useState } from 'react';
 import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 
 function Orders() {
 
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [products, setProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] =  useState<OrderLocationData>();
 
@@ -17,12 +21,32 @@ function Orders() {
        .catch(error => console.log(error));
     }, []);
 
+
+    const handleSelectProduct = (product: Product) => {
+    
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
+       <>
         <div className="orders-container">
             <StepsHeader/>
-            <ProductList products={products}/>
+            <ProductList
+             products={products}
+             onSelectProduct={handleSelectProduct}
+             selectedProducts={selectedProducts}/>
             <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
+            <OrderSummary/>
         </div>
+        <Footer/>
+       </>
     );
 }
 export default Orders;
